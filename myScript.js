5576c692b82fb3d45be14831401eefe7643da764
mmayhall <55414339+mmayhall@users.noreply.github.com>
2019-09-18 16:01:56 -0500

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,122 @@
+
+function openNav() {
+    document.getElementById("sideNavigation").style.width = "100%";
+    document.getElementById("main").style.marginRight = "10%";
+}
+ 
+function closeNav() {
+    document.getElementById("sideNavigation").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+		$(document).ready(function(){
+		  $('.banner-to-the-fairest').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+
+		$(document).ready(function(){
+		  $('.banner-the-seas').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+
+		$(document).ready(function(){
+		  $('.banner-hue-series').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+
+		$(document).ready(function(){
+		  $('.banner-plantslaughter').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+
+		$(document).ready(function(){
+		  $('.banner-cookbook').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+
+		$(document).ready(function(){
+		  $('.banner-freakshow').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+
+		$(document).ready(function(){
+		  $('.banner-apl').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+
+		$(document).ready(function(){
+		  $('.banner-chess').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+
+		$(document).ready(function(){
+		  $('.banner-getrecd').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+
+		$(document).ready(function(){
+		  $('.banner-seasonal-motion-graphics').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+
+		$(document).ready(function(){
+		  $('.banner-wtbib').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+
+		$(document).ready(function(){
+		  $('.banner-jotb').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+
+		$(document).ready(function(){
+		  $('.banner-gale-rebrand').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+
+		$(document).ready(function(){
+		  $('.banner-foxygen').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
