3852969c9f82f659cfa4c5506395e8b43195b792
mmayhall <55414339+mmayhall@users.noreply.github.com>
2019-10-07 00:33:54 -0500

    update with type west mousemove

--- myScript.js
+++ myScript.js
@@ -120,3 +120,11 @@ function closeNav() {
 			$(this).css('background-position', x + 'px ' + y + 'px');
 		  });    
 		});
+
+		$(document).ready(function(){
+		  $('.banner-typewest').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
