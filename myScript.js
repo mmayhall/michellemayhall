c8089ff3cabecca315884b048527d6ce0b1c8d83
mmayhall <55414339+mmayhall@users.noreply.github.com>
2019-11-09 16:33:40 -0600

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -120,9 +120,29 @@ function closeNav() {
 			$(this).css('background-position', x + 'px ' + y + 'px');
 		  });    
 		});
-
 		$(document).ready(function(){
 		  $('.banner-typewest').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+		$(document).ready(function(){
+		  $('.banner-disney').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+		$(document).ready(function(){
+		  $('.banner-aurec').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
+		$(document).ready(function(){
+		  $('.banner-saurus').mousemove(function(e){
 			var x = -(e.pageX + this.offsetLeft) / 20;
 			var y = -(e.pageY + this.offsetTop) / 20;
 			$(this).css('background-position', x + 'px ' + y + 'px');
