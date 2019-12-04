6734a75adf898ae559bd3b482ac69fe6448f8690
mmayhall <55414339+mmayhall@users.noreply.github.com>
2019-12-03 20:41:47 -0600

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -148,3 +148,10 @@ function closeNav() {
 			$(this).css('background-position', x + 'px ' + y + 'px');
 		  });    
 		});
+		$(document).ready(function(){
+		  $('.banner-eroc').mousemove(function(e){
+			var x = -(e.pageX + this.offsetLeft) / 20;
+			var y = -(e.pageY + this.offsetTop) / 20;
+			$(this).css('background-position', x + 'px ' + y + 'px');
+		  });    
+		});
